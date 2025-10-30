84478712fa8592ac2201b9eb89f62e7018c37231
Mehdi Rhifar <62858238+MehdiRhifar@users.noreply.github.com>
2025-10-30 22:43:50 +0100

    Add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 const rootElement = document.getElementById('root')
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
       <BrowserRouter>
+        <Analytics />
+        <SpeedInsights />
         <App />
       </BrowserRouter>
     </React.StrictMode>
